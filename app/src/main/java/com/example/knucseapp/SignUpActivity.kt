95fc2a58ac78393e1912d330f11b601c709fb8fa
Jihae Na <njnjh0317@gmail.com>
2021-07-10 23:45:44 +0900

    set error message

--- app/src/main/java/com/example/knucseapp/SignUpActivity.kt
+++ app/src/main/java/com/example/knucseapp/SignUpActivity.kt
@@ -36,7 +36,11 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     private fun textWatcher() {
-        binding.signupStdIdTextview.addTextChangedListener(object : TextWatcher{
+
+        val sentence1 = "필수입력 항목 입니다."
+
+        //이메일 입력
+        binding.emailText.addTextChangedListener(object : TextWatcher{
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
             }
@@ -47,11 +51,100 @@ class SignUpActivity : AppCompatActivity() {
 
             override fun afterTextChanged(s: Editable?) {
                 Log.d("SignupActivity", "call textwatcher")
-                if(binding.signupStdIdTextview.text!!.isEmpty()){
-                    binding.signupStdIdLayout.error = "필수입력!!"
+                if(binding.emailText.text!!.isEmpty()){
+                    binding.emailLayout.error = sentence1
                 }
                 else{
-                    binding.signupStdIdLayout.error = null
+                    binding.emailLayout.error = null
+                }
+            }
+
+        })
+
+        //비번
+        binding.pwText.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.d("SignupActivity", "call textwatcher")
+                if(binding.pwText.text!!.isEmpty()){
+                    binding.pwLayout.error = sentence1
+                }
+                else{
+                    binding.pwLayout.error = null
+                }
+            }
+
+        })
+
+        //비번 재확인
+        binding.ckpwText.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.d("SignupActivity", "call textwatcher")
+                if(binding.ckpwText.text!!.isEmpty()){
+                    binding.ckpwLayout.error = sentence1
+                }
+                else{
+                    binding.ckpwLayout.error = null
+                }
+            }
+
+        })
+
+
+        //학번 입력
+        binding.stdIdText.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.d("SignupActivity", "call textwatcher")
+                if(binding.stdIdText.text!!.isEmpty()){
+                    binding.stdIdLayout.error = sentence1
+                }
+                else{
+                    binding.stdIdLayout.error = null
+                }
+            }
+
+        })
+
+        //이름입력
+        binding.nameText.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.d("SignupActivity", "call textwatcher")
+                if(binding.nameText.text!!.isEmpty()){
+                    binding.nameLayout.error = sentence1
+                }
+                else{
+                    binding.nameLayout.error = null
                 }
             }
 
