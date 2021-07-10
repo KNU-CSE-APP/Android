66381044a41793cb140dba6604b2091ac61650a8
Jihae Na <njnjh0317@gmail.com>
2021-07-10 23:27:11 +0900

    [#1] update signup view

--- app/src/main/java/com/example/knucseapp/SignUpActivity.kt
+++ app/src/main/java/com/example/knucseapp/SignUpActivity.kt
@@ -2,8 +2,13 @@ package com.example.knucseapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.MenuItem
 import com.example.knucseapp.databinding.ActivitySignUpBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -16,6 +21,7 @@ class SignUpActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        textWatcher()
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
@@ -25,6 +31,30 @@ class SignUpActivity : AppCompatActivity() {
                 return true
             }
         }
+
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun textWatcher() {
+        binding.signupStdIdTextview.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.d("SignupActivity", "call textwatcher")
+                if(binding.signupStdIdTextview.text!!.isEmpty()){
+                    binding.signupStdIdLayout.error = "필수입력!!"
+                }
+                else{
+                    binding.signupStdIdLayout.error = null
+                }
+            }
+
+        })
     }
 }
