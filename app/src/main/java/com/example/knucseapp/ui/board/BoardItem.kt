45b2bce1c4f8f51a3b7653a77342d3fdbe1732c6
cheonjiwan <giyan1000@naver.com>
2021-07-12 18:45:37 +0900

    [#4] set board list layout

---
+++ app/src/main/java/com/example/knucseapp/ui/board/BoardItem.kt
@@ -0,0 +1,9 @@
+package com.example.knucseapp.ui.board
+
+data class BoardItem(
+        val author: String,
+        val title: String,
+        val content: String,
+        val date : String,
+        val CommentCnt : String
+)
