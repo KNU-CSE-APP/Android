47d9077ac33b51ef19d861066960e86c77ada886
Jihae Na <njnjh0317@gmail.com>
2021-07-25 21:08:20 +0900

    [#4] set logout

--- app/src/main/java/com/example/knucseapp/ui/mypage/MyPageAdapter.kt
+++ app/src/main/java/com/example/knucseapp/ui/mypage/MyPageAdapter.kt
@@ -1,11 +1,15 @@
 package com.example.knucseapp.ui.mypage
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.example.knucseapp.databinding.MypageRecyclerBinding
+import com.example.knucseapp.ui.SignInActivity
 import com.example.knucseapp.ui.mypage.menu.ReservationHistoryActivity
 import com.example.knucseapp.ui.mypage.menu.SettingActivity
 
@@ -29,7 +33,7 @@ class MyPageAdapter : RecyclerView.Adapter<Holder>() {
 class Holder(val binding: MypageRecyclerBinding) : RecyclerView.ViewHolder(binding.root) {
     init {
         binding.root.setOnClickListener{
-            Toast.makeText(binding.root.context, "클릭된 아이템 = ${binding.menutext.text}", Toast.LENGTH_LONG).show()
+
             when(binding.menutext.text)
             {
                 Menuname.grouplist[0] -> {
@@ -41,6 +45,23 @@ class Holder(val binding: MypageRecyclerBinding) : RecyclerView.ViewHolder(bindi
                     val intent = Intent(it.context, SettingActivity::class.java)
                     intent.putExtra("menu_name", Menuname.grouplist[4])
                     it.context.startActivity(intent)
+                }
+                Menuname.grouplist[5] -> {
+                    AlertDialog.Builder(it.context)
+                            .setTitle("로그아웃")
+                            .setMessage("로그아웃하시겠습니까?")
+                            .setPositiveButton("OK") { _, _ ->
+                                val intent = Intent(it.context, SignInActivity::class.java)
+                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                it.context.startActivity(intent)
+                            }
+                            .setNegativeButton("CANCEL") { _, _ -> // 취소시 처리 로직
+                            }
+                            .show()
+
+                }
+                else -> {
+                    Toast.makeText(binding.root.context, "클릭된 아이템 = ${binding.menutext.text}", Toast.LENGTH_LONG).show()
                 }
             }
         }
