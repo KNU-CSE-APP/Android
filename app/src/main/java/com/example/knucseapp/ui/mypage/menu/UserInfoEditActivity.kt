b06a4e55225dfc65ef95a813e1d260dfd3cefff2
cheonjiwan <giyan1000@naver.com>
2021-07-31 15:19:03 +0900

    [#4] glide를 사용한 프로필이미지 변경 작업

--- app/src/main/java/com/example/knucseapp/ui/mypage/menu/UserInfoEditActivity.kt
+++ app/src/main/java/com/example/knucseapp/ui/mypage/menu/UserInfoEditActivity.kt
@@ -1,23 +1,29 @@
 package com.example.knucseapp.ui.mypage.menu
 
-import android.graphics.Color
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.MenuItem
 import android.view.View
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.example.knucseapp.R
-import com.example.knucseapp.R.color.knu_red
 import com.example.knucseapp.databinding.ActivityUserInfoEditBinding
 
 class UserInfoEditActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityUserInfoEditBinding
     lateinit var nickName : String
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            Glide.with(this).load(it.data?.data).into(binding.accountIvProfile)
+            binding.btnOk.isEnabled=true
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,20 +34,6 @@ class UserInfoEditActivity : AppCompatActivity() {
         nickName = binding.userNicknameText.text.toString()
         binding.btnOk.isEnabled = false
 
-        binding.userNicknameEdit.addTextChangedListener(object : TextWatcher{
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-            }
-            override fun afterTextChanged(s: Editable?) {
-                if (!binding.userNicknameEdit.text.toString().equals(nickName)){
-                    binding.btnOk.isEnabled = true
-                }
-            }
-        })
         binding.apply {
             btnOk.setOnClickListener {
 
@@ -52,13 +44,39 @@ class UserInfoEditActivity : AppCompatActivity() {
                 binding.userNicknameEdit.visibility = View.VISIBLE
                 binding.userNicknameEdit.setText(binding.userNicknameText.text)
             }
+            userNicknameEdit.addTextChangedListener(object : TextWatcher{
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                }
+                override fun afterTextChanged(s: Editable?) {
+                    if (!binding.userNicknameEdit.text.toString().equals(nickName)){
+                        binding.btnOk.isEnabled = true
+                    }
+                }
+            })
         }
     }
 
     private fun showDialog(){
-        val builder = AlertDialog.Builder(this)
+        val builder = AlertDialog.Builder(this).create()
         val dialogView = layoutInflater.inflate(R.layout.profile_edit_dialog,null)
-        builder.setView(dialogView).show()
+        val tv_changeProfile = dialogView.findViewById<TextView>(R.id.change_profile)
+        val tv_removeProfile = dialogView.findViewById<TextView>(R.id.change_profile)
+
+        tv_changeProfile.setOnClickListener {
+            var photoPickerIntent = Intent(Intent.ACTION_PICK)
+            photoPickerIntent.type = "image/*"
+            getContent.launch(photoPickerIntent)
+            builder.dismiss()
+        }
+
+        builder.setView(dialogView)
+        builder.show()
+
     }
 
     private fun setToolbar(){
