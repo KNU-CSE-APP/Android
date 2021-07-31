a2bc00ab2ba01d381f9fe10e0241daa984cd1fe2
Jihae Na <njnjh0317@gmail.com>
2021-07-31 17:02:48 +0900

    edit button design

--- app/src/main/java/com/example/knucseapp/ui/SignUpActivity.kt
+++ app/src/main/java/com/example/knucseapp/ui/SignUpActivity.kt
@@ -6,6 +6,7 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.knucseapp.R
 import com.example.knucseapp.databinding.ActivitySignInBinding
@@ -22,6 +23,7 @@ class SignUpActivity : AppCompatActivity() {
 
         setToolbar()
         textWatcher()
+        setButton()
     }
     private fun setToolbar(){
         setSupportActionBar(binding.toolbar)
@@ -40,133 +42,137 @@ class SignUpActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    private fun setButton() {
+        binding.btnSignup.setOnClickListener {
+            //TODO: 입력 칸 확인
+            Toast.makeText(this, "가입한 정보로 로그인 해 주세요.", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
+
     private fun textWatcher() {
 
         val sentence1 = "필수입력 항목 입니다."
 
-        //이메일 입력
-        binding.emailText.addTextChangedListener(object : TextWatcher{
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                if(binding.emailText.text!!.isEmpty()){
-                    binding.emailLayout.error = sentence1
-                }
-                else{
-                    binding.emailLayout.error = null
-                }
-            }
-
-        })
-
-        //비번
-        binding.pwText.addTextChangedListener(object : TextWatcher{
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                if(binding.pwText.text!!.isEmpty()){
-                    binding.pwLayout.error = sentence1
-                }
-                else{
-                    binding.pwLayout.error = null
-                }
-            }
-
-        })
-
-        //비번 재확인
-        binding.ckpwText.addTextChangedListener(object : TextWatcher{
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                if(binding.ckpwText.text!!.isEmpty()){
-                    binding.ckpwLayout.error = sentence1
-                }
-                else{
-                    binding.ckpwLayout.error = null
-                }
-            }
-
-        })
-
-
-        //학번 입력
-        binding.stdIdText.addTextChangedListener(object : TextWatcher{
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                if(binding.stdIdText.text!!.isEmpty()){
-                    binding.stdIdLayout.error = sentence1
-                }
-                else{
-                    binding.stdIdLayout.error = null
-                }
-            }
-
-        })
-
-        //이름입력
-        binding.nameText.addTextChangedListener(object : TextWatcher{
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                if(binding.nameText.text!!.isEmpty()){
-                    binding.nameLayout.error = sentence1
-                }
-                else{
-                    binding.nameLayout.error = null
-                }
-            }
-
-        })
-
-        binding.nicknameText.addTextChangedListener(object : TextWatcher{
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                if(binding.nicknameText.text!!.isEmpty()){
-                    binding.nicknameLayout.error = sentence1
-                }
-                else{
-                    binding.nicknameLayout.error = null
+        binding.apply {
+            //이메일
+            emailText.addTextChangedListener(object : TextWatcher{
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 }
 
-            }
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                }
 
-        })
+                override fun afterTextChanged(s: Editable?) {
+                    if(binding.emailText.text!!.isEmpty()){
+                        binding.emailLayout.error = sentence1
+                    }
+                    else{
+                        binding.emailLayout.error = null
+                    }
+                }
+
+            })
+
+            //비밀번호
+            pwText.addTextChangedListener(object : TextWatcher{
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    if(binding.pwText.text!!.isEmpty()){
+                        binding.pwLayout.error = sentence1
+                    }
+                    else{
+                        binding.pwLayout.error = null
+                    }
+                }
+            })
+
+            //비번 재확인
+            ckpwText.addTextChangedListener(object : TextWatcher{
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    if(binding.ckpwText.text!!.isEmpty()){
+                        binding.ckpwLayout.error = sentence1
+                    }
+                    else if(!(binding.ckpwText.text.toString()!!.equals(binding.pwText.text.toString()))) {
+                        binding.ckpwLayout.error = "일치하지 않습니다."
+                    }
+                    else{
+                        binding.ckpwLayout.error = null
+                    }
+                }
+
+            })
+
+            //학번
+            stdIdText.addTextChangedListener(object : TextWatcher{
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    if(binding.stdIdText.text!!.isEmpty()){
+                        binding.stdIdLayout.error = sentence1
+                    }
+                    else{
+                        binding.stdIdLayout.error = null
+                    }
+                }
+
+            })
+
+            //이름
+            nameText.addTextChangedListener(object : TextWatcher{
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    if(binding.nameText.text!!.isEmpty()){
+                        binding.nameLayout.error = sentence1
+                    }
+                    else{
+                        binding.nameLayout.error = null
+                    }
+                }
+            })
+
+            //닉네임
+            nicknameText.addTextChangedListener(object : TextWatcher{
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    if(binding.nicknameText.text!!.isEmpty()){
+                        binding.nicknameLayout.error = sentence1
+                    }
+                    else{
+                        binding.nicknameLayout.error = null
+                    }
+
+                }
+
+            })
+        }
+
     }
 }
