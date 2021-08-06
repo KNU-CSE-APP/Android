1df401065be69051967aebe6346e28ed6ffbc9e7
cheonjiwan <giyan1000@naver.com>
2021-08-06 17:24:02 +0900

    [#28] add cookiemanager

--- app/src/main/java/com/example/knucseapp/data/request/ApiRequestFactory.kt
+++ app/src/main/java/com/example/knucseapp/data/request/ApiRequestFactory.kt
@@ -1,10 +1,13 @@
 package com.example.knucseapp.data.request.user
 
+import okhttp3.CookieJar
 import okhttp3.Interceptor
+import okhttp3.JavaNetCookieJar
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.net.CookieManager
 
 object ApiRequestFactory {
     private const val baseUrl = "http://3.34.14.12"
@@ -22,11 +25,12 @@ object ApiRequestFactory {
         .addConverterFactory(GsonConverterFactory.create())
         .client(OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().apply { this.level = HttpLoggingInterceptor.Level.BODY })
-            .addInterceptor(headerInterceptor).build())
-        .build()
+            .addInterceptor(headerInterceptor)
+            .cookieJar(JavaNetCookieJar(CookieManager()))
+            .build()
+        ).build()
 
     val userService : UserService by lazy{
         retrofit.create(UserService::class.java)
     }
-
 }
