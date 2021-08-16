52a9c4e85cde57a09f145a52d841e85e70544496
Jihae Na <njnjh0317@gmail.com>
2021-08-16 21:32:07 +0900

    [#29] 작성자에게만 게시글, 댓글 삭제 버튼 보이도록 수정

--- app/src/main/java/com/example/knucseapp/ui/board/detail/ReplyAdapter.kt
+++ app/src/main/java/com/example/knucseapp/ui/board/detail/ReplyAdapter.kt
@@ -3,10 +3,12 @@ package com.example.knucseapp.ui.board.detail
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.knucseapp.data.model.CommentDTO
 import com.example.knucseapp.databinding.ReplyRecyclerBinding
+import com.example.knucseapp.ui.util.MyApplication
 import okhttp3.internal.notify
 
 class ReplyAdapter(var link: CommentActivity.reply?) : RecyclerView.Adapter<ReplyHolder>() {
@@ -25,12 +27,12 @@ class ReplyAdapter(var link: CommentActivity.reply?) : RecyclerView.Adapter<Repl
     override fun onBindViewHolder(holder: ReplyHolder, position: Int) {
         val comment = replys.get(position)
         holder.setReply(comment)
-        if(link == null){
-            holder.binding.imgSetting.visibility = INVISIBLE
-        }
-        else {
-            holder.binding.imgSetting.setOnClickListener {
-                link?.callPopupMenu(comment.commentId, holder.binding.imgSetting)
+        if(link!=null && comment.author.equals(MyApplication.prefs.getUserNickname())) {
+            holder.binding.imgSetting.run {
+                visibility = VISIBLE
+                setOnClickListener{
+                    link?.callPopupMenu(comment.commentId, holder.binding.imgSetting)
+                }
             }
         }
     }
