a9e1d9fb480c9280343232fb89bd0cf1cf7d350c
Jihae Na <njnjh0317@gmail.com>
2021-08-18 14:07:26 +0900

    [#35] api setting

--- app/src/main/java/com/example/knucseapp/data/repository/ReservationRepository.kt
+++ app/src/main/java/com/example/knucseapp/data/repository/ReservationRepository.kt
@@ -1,4 +1,20 @@
 package com.example.knucseapp.data.repository
 
+import com.example.knucseapp.data.model.ReservationDTO
+import com.example.knucseapp.data.request.user.ApiRequestFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 class ReservationRepository {
+
+    suspend fun classState(building: String, roomNumber: Int) = withContext(Dispatchers.IO) {
+        ApiRequestFactory.reservationService.classState(building, roomNumber)
+    }
+    suspend fun makeReservation(reservationDTO: ReservationDTO) = withContext(Dispatchers.IO) {
+        ApiRequestFactory.reservationService.makeReservation(reservationDTO)
+    }
+
+    suspend fun myReservation() = withContext(Dispatchers.IO) {
+        ApiRequestFactory.reservationService.myReservation()
+    }
 }
--- app/src/main/java/com/example/knucseapp/data/request/ReservationService.kt
+++ app/src/main/java/com/example/knucseapp/data/request/ReservationService.kt
@@ -1,4 +1,19 @@
 package com.example.knucseapp.data.request
 
+import com.example.knucseapp.data.model.*
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
 interface ReservationService {
+
+    @GET("classRoom/searchSeats/{building}/{roomNumber}")
+    fun classState(@Path("building") building: String, @Path("roomNumber") roomNumber: Int): ApiResult<List<ClassSeatDTO>>
+
+    @POST("/reservation/reservation")
+    fun makeReservation(@Body reservationDTO: ReservationDTO): ApiResult<String>
+
+    @POST("reservation/findReservation")
+    fun myReservation(): ApiResult<FindReservationDTO>
 }
