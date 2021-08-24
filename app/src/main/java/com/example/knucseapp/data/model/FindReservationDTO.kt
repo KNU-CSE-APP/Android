3dbcb83139bc688c5c3556cb7dd56cedbdaa5f02
cheonjiwan <giyan1000@naver.com>
2021-08-24 20:48:13 +0900

    [#35] FIX : 예약정보확인 뷰에서 입,퇴실 시간 / 예약내역 뷰에서 연장횟수, 입,퇴실 시간 추가

--- app/src/main/java/com/example/knucseapp/data/model/FindReservationDTO.kt
+++ app/src/main/java/com/example/knucseapp/data/model/FindReservationDTO.kt
@@ -3,5 +3,8 @@ package com.example.knucseapp.data.model
 data class FindReservationDTO(
         val building: String,
         val roomNumber: Int,
-        val seatNumber: Int
+        val seatNumber: Int,
+        val dueDate : String,
+        val startDate : String,
+        val extensionNum : String
 )
