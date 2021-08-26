4a29f6c5f22e0aab8ec30182729ebd8a0306604b
Jihae Na <njnjh0317@gmail.com>
2021-08-27 01:52:57 +0900

    [#29] 이미지 클릭시 popup 띄우기

--- app/src/main/java/com/example/knucseapp/ui/board/detail/ContentPhotoAdapter.kt
+++ app/src/main/java/com/example/knucseapp/ui/board/detail/ContentPhotoAdapter.kt
@@ -2,14 +2,18 @@ package com.example.knucseapp.ui.board.detail
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.ceylonlabs.imageviewpopup.ImagePopup
 import com.example.knucseapp.R
 import com.example.knucseapp.databinding.PhotoRecyclerBinding
+import kotlinx.android.synthetic.main.seat_image_dialog.view.*
 
 class ContentPhotoAdapter :RecyclerView.Adapter<ContentPhotoAdapter.Holder>() {
 
     var imageurl = mutableListOf<String>()
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val binding = PhotoRecyclerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -18,6 +22,14 @@ class ContentPhotoAdapter :RecyclerView.Adapter<ContentPhotoAdapter.Holder>() {
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         val url = imageurl.get(position)
+        holder.binding.contentImageview.setOnClickListener {
+            val mDialogView = LayoutInflater.from(holder.binding.root.context).inflate(R.layout.seat_image_dialog, null)
+            Glide.with(holder.binding.root.context).load(url).into(mDialogView.imageview)
+            val mBuilder = AlertDialog.Builder(holder.binding.root.context)
+                .setView(mDialogView)
+            mBuilder.show()
+
+        }
         holder.setSetting(url)
     }
 
